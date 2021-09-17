60c3428e2610c34940d75eac5cc0b4e30488ccf0
Eslam Emara <emara.beats@gmail.com>
2021-09-17 14:46:34 +0200

    initial commit

--- AirConditioner/APP/App.h
+++ AirConditioner/APP/App.h
@@ -3,25 +3,58 @@
  *
  * Created: 9/17/2021 1:35:42 PM
  *  Author: OverFlow
- */ 
+ */
 
 
 #ifndef APP_H_
 #define APP_H_
-
+/*=================MAHMOUD=============*/
 
 void App_Init();														/*Init ECUAL*/
 
 uint8_t App_GetUserInput();												/*Get desired temperature from keypad (integer)*/
+
+/* limits on inputs > 15 , <30*/
+/*read from keypad until = is pressed*/
+
 uint8_t App_MeasureCurrentTemp();										/*Get current sensor readings*/
+/*get sensor readings (current temprature) */
 
 void App_AdjustTemp(uint8_t currentTemp ,uint8_t desired);				/*Adjust fan according to desired temp*/
 
+/*if currentTemp > desired FAN On*/
+/*else Fan OFF*/
+
+static void ConvertIntToStr(uint8_t number);				/*convert integer to string 2 digits*/
+/*
+void convert_2DigitInt_to_str(unsigned char number,char* arr){
+	arr[0] = (number / 10);
+
+	if (arr[0] == 0){
+		arr[0] = number + '0';
+		arr[1] = '\0';
+		return;
+	}
+	else{
+		arr[1]= (number - (arr[0]*10));
+		arr[0]+= '0';
+		arr[1]+='0';
+		arr[2]= '\0';
+	}
+}*/
+
+/*=============ESLAM==========*/
+
 void App_PrintCurrenTemp(uint8_t current);								/*Print Temp values on LCD*/
-void App_PrintDesiredMode(uint8_t desired);
-
-
-			
 /*============ convert to string ==============*/
+
+void App_PrintDesiredMode(uint8_t desired);
+/*============ convert to string ==============*/
+
+void app();
+
+
+
+
 
 #endif /* APP_H_ */
